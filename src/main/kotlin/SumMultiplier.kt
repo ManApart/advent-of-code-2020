8049144214e83b4f86036bb0ce38f30c8c7a1581
ManApart <raustinkucera@gmail.com>
2020-12-01 17:16:10 -0500

    use list instead of pair

--- src/main/kotlin/SumMultiplier.kt
+++ src/main/kotlin/SumMultiplier.kt
@@ -1,15 +1,15 @@
 fun findAndMultiply(targetSum: Int, numbers: List<Int>): Int {
     val matches = findSumMultiplier(targetSum, numbers)
-    return matches.first * matches.second
+    return matches.reduce {acc: Int, i: Int -> i * acc }
 }
 
-fun findSumMultiplier(targetSum: Int, numbers: List<Int>): Pair<Int, Int> {
+fun findSumMultiplier(targetSum: Int, numbers: List<Int>): List<Int> {
     numbers.forEach { first ->
         numbers.forEach { second ->
             if (targetSum == first + second) {
-                return Pair(first, second)
+                return listOf(first, second)
             }
         }
     }
-    return Pair(0, 0)
+    return listOf()
 }
--- src/test/kotlin/SumMultiplierTest.kt
+++ src/test/kotlin/SumMultiplierTest.kt
@@ -6,13 +6,13 @@ class SumMultiplierTest {
     @Test
     fun findSimpleSum() {
         val actual = findSumMultiplier(2, listOf(1, 1))
-        assertEquals(Pair(1, 1), actual)
+        assertEquals(listOf(1, 1), actual)
     }
 
     @Test
     fun findSumFromListOfThree() {
         val actual = findSumMultiplier(2, listOf(1, 1, 2))
-        assertEquals(Pair(1, 1), actual)
+        assertEquals(listOf(1, 1), actual)
     }
 
     @Test
