da87b698b873790fd12ca8b5baf2e0a3ee3620a2
ManApart <raustinkucera@gmail.com>
2020-12-03 16:31:15 -0500

    parse grid

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -1,2 +1,22 @@
-class Grid() {
+class Grid(lines: List<String>) {
+    private val map = buildMap(lines)
+
+    private fun buildMap(lines: List<String>): Map<Int, Map<Int, Boolean>> {
+        val map = mutableMapOf<Int, MutableMap<Int, Boolean>>()
+        val totalLines = lines.size-1
+        lines.forEachIndexed { lineNumber: Int, line: String ->
+            val y = totalLines - lineNumber
+            line.toCharArray().forEachIndexed { x: Int, char ->
+                val isTree = (char == '#')
+                map.putIfAbsent(x, mutableMapOf())
+                map[x]!![y] = isTree
+            }
+        }
+
+        return map
+    }
+
+    fun isTree(x: Int, y: Int): Boolean {
+        return map[x]?.get(y) ?: false
+    }
 }
--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -1,2 +1,22 @@
+import org.testng.Assert.assertFalse
+import org.testng.annotations.Test
+import kotlin.test.assertTrue
+
 class GridTest {
+
+    @Test
+    fun parsingAndCoordinateSystem() {
+        val input = listOf(
+            "..",
+            ".#"
+        )
+
+        val grid = Grid(input)
+
+        assertFalse(grid.isTree(0,1))
+        assertFalse(grid.isTree(1,1))
+        assertFalse(grid.isTree(0,0))
+        assertTrue(grid.isTree(1,0))
+
+    }
 }
