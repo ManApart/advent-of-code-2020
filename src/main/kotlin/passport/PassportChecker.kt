d170a79771079d216f5bfc3b1aea7d45332e5240
ManApart <raustinkucera@gmail.com>
2020-12-04 17:18:42 -0500

    count valid passports example tests

--- src/main/kotlin/passport/PassportChecker.kt
+++ src/main/kotlin/passport/PassportChecker.kt
@@ -3,3 +3,7 @@ import passport.Passport
 fun countPassportsWithAllKeys(lines: List<String>): Int {
     return parsePassports(lines).map { Passport(it) }.count { it.hasAllKeys() }
 }
+
+fun countValidPassports(lines: List<String>): Int {
+    return parsePassports(lines).map { Passport(it) }.count { it.isValid() }
+}
