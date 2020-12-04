656d636bb0985d7fe290bec288754f1105c2170a
ManApart <raustinkucera@gmail.com>
2020-12-04 15:44:57 -0500

    don't add blank passports

--- src/main/kotlin/PassportParser.kt
+++ src/main/kotlin/PassportParser.kt
@@ -2,18 +2,22 @@ fun parsePassports(lines: List<String>): List<List<String>> {
     val results = mutableListOf<List<String>>()
     var passport = ""
     lines.forEach { line ->
-        if (line.isBlank()){
-            results.add(passport.toChunks())
+        if (line.isBlank()) {
+            if (passport.isNotBlank()) {
+                results.add(passport.toChunks())
+            }
             passport = ""
         } else {
             passport += " ${line.trim()}"
         }
     }
-    results.add(passport.toChunks())
+    if (passport.isNotBlank()) {
+        results.add(passport.toChunks())
+    }
     return results
 
 }
 
-private fun String.toChunks() : List<String> {
+private fun String.toChunks(): List<String> {
     return this.trim().split(" ")
 }
--- src/test/kotlin/PassportParserTest.kt
+++ src/test/kotlin/PassportParserTest.kt
@@ -80,4 +80,23 @@ class PassportParserTest {
         assertEquals("iyr:2013", results.last().first())
         assertEquals("byr:1929", results.last().last())
     }
+
+    @Test
+    fun parseMultiplePassportsDoesNotCountBlanks() {
+    val input = """
+        
+        
+        ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm
+        
+        iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929
+        
+        
+        """.trimIndent().split("\n")
+
+        val results = parsePassports(input)
+
+        assertEquals(2, results.size)
+        assertEquals(8, results.first().size)
+        assertEquals(7, results.last().size)
+    }
 }
