96f80193871afd67b5801bbeca074ebddc7a67a5
ManApart <raustinkucera@gmail.com>
2020-12-04 15:47:09 -0500

    full example parse

--- src/test/kotlin/PassportParserTest.kt
+++ src/test/kotlin/PassportParserTest.kt
@@ -2,21 +2,6 @@ import org.testng.annotations.Test
 import kotlin.test.assertEquals
 
 class PassportParserTest {
-//    val input = """
-//        ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-//        byr:1937 iyr:2017 cid:147 hgt:183cm
-//
-//        iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-//        hcl:#cfa07d byr:1929
-//
-//        hcl:#ae17e1 iyr:2013
-//        eyr:2024
-//        ecl:brn pid:760753108 byr:1931
-//        hgt:179cm
-//
-//        hcl:#cfa07d eyr:2025 pid:166559648
-//        iyr:2011 ecl:brn hgt:59in
-//    """.trimIndent()
 
     @Test
     fun parseSingleLine() {
@@ -99,4 +84,28 @@ class PassportParserTest {
         assertEquals(8, results.first().size)
         assertEquals(7, results.last().size)
     }
+
+    @Test
+    fun fullParseTest() {
+        val input = """
+        ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+        byr:1937 iyr:2017 cid:147 hgt:183cm
+
+        iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+        hcl:#cfa07d byr:1929
+
+        hcl:#ae17e1 iyr:2013
+        eyr:2024
+        ecl:brn pid:760753108 byr:1931
+        hgt:179cm
+
+        hcl:#cfa07d eyr:2025 pid:166559648
+        iyr:2011 ecl:brn hgt:59in
+    """.trimIndent().split("\n")
+
+        val results = parsePassports(input)
+
+        assertEquals(4, results.size)
+    }
+
 }
