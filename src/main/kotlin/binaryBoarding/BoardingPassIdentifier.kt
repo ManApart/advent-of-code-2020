5875ea7b4686fe53d74dbf474c2dc1312bc06d02
ManApart <raustinkucera@gmail.com>
2020-12-05 08:39:06 -0500

    consolidate code

--- src/main/kotlin/binaryBoarding/BoardingPassIdentifier.kt
+++ src/main/kotlin/binaryBoarding/BoardingPassIdentifier.kt
@@ -1,7 +1,5 @@
 package binaryBoarding
 
-import findNumber
-
 fun findLargestSeatID(pickLargerSteps: List<List<Boolean>>): Int {
     return pickLargerSteps.map { identifySeatID(it) }.maxOrNull() ?: 0
 }
@@ -11,4 +9,18 @@ fun identifySeatID(pickLargerSteps: List<Boolean>): Int {
     val column = findNumber(0, 7, pickLargerSteps.subList(7, 10))
 
     return row * 8 + column
+}
+
+fun findNumber(absoluteMin: Int, absoluteMax: Int, pickLargerSteps: List<Boolean>): Int {
+    var min = absoluteMin
+    var max = absoluteMax + 1
+    pickLargerSteps.forEach {
+        val step = (max-min)/2
+        if (it){
+            min += step
+        } else {
+            max -= step
+        }
+    }
+    return min
 }
