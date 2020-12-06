da9f8bb75e66848836263dd65dee5bc78e758d94
ManApart <raustinkucera@gmail.com>
2020-12-06 09:33:08 -0500

    parse customs

--- src/main/kotlin/customs/CustomsParser.kt
+++ src/main/kotlin/customs/CustomsParser.kt
@@ -1,2 +1,21 @@
 package customs
 
+fun parseCustoms(lines: List<String>) : List<List<String>> {
+    val results = mutableListOf<List<String>>()
+    var group = mutableListOf<String>()
+    lines.forEach { line ->
+        if (line.isBlank()) {
+            if (group.isNotEmpty()){
+                results.add(group)
+            }
+            group = mutableListOf()
+        } else {
+            group.add(line.trim())
+        }
+    }
+    if (group.isNotEmpty()){
+        results.add(group)
+    }
+
+    return results
+}
--- src/test/kotlin/customs/CustomsParserTest.kt
+++ src/test/kotlin/customs/CustomsParserTest.kt
@@ -1,4 +1,66 @@
 package customs
 
+import org.testng.annotations.Test
+import kotlin.test.assertEquals
+
 class CustomsParserTest {
+
+    @Test
+    fun parseSingleLine() {
+        val input = "abcx"
+
+        val result = parseCustoms(listOf(input))
+
+        //groups
+        assertEquals(1, result.size)
+
+        //people in group
+        assertEquals(1, result.first().size)
+
+        assertEquals("abcx", result.first().first())
+    }
+
+    @Test
+    fun parseSingleGroup() {
+        val input = """
+            abcx
+            abcy
+            abcz
+        """.trimIndent().split("\n")
+
+        val result = parseCustoms(input)
+
+        //groups
+        assertEquals(1, result.size)
+
+        //people in group
+        assertEquals(3, result.first().size)
+
+        assertEquals("abcx", result.first()[0])
+        assertEquals("abcy", result.first()[1])
+        assertEquals("abcz", result.first()[2])
+    }
+
+    @Test
+    fun parseMultiples() {
+        val input = """
+            abcx
+            
+            abcz
+        """.trimIndent().split("\n")
+
+        val result = parseCustoms(input)
+
+        //groups
+        assertEquals(2, result.size)
+
+        //people in group
+        assertEquals(1, result.first().size)
+        assertEquals(1, result.last().size)
+
+        assertEquals("abcx", result.first()[0])
+        assertEquals("abcz", result.last()[0])
+    }
+
+
 }
