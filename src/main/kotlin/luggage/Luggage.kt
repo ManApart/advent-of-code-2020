25f70e66ffb2b0b8a34ee5591b15bc690d5262f3
ManApart <raustinkucera@gmail.com>
2020-12-07 16:54:14 -0500

    add all ancestors

--- src/main/kotlin/luggage/Luggage.kt
+++ src/main/kotlin/luggage/Luggage.kt
@@ -9,4 +9,18 @@ class Luggage(bags: List<Bag>) {
         }
     }
 
+    fun getAllAncestors(bagId: String): List<Bag> {
+        val ancestors = mutableSetOf<Bag>()
+        val open = bags[bagId]!!.parents
+
+        while (open.isNotEmpty()){
+            val current = open.first()
+            open.remove(current)
+            ancestors.add(current)
+            open.addAll(current.parents)
+        }
+
+        return ancestors.toList()
+    }
+
 }
--- src/test/kotlin/luggage/LuggageTest.kt
+++ src/test/kotlin/luggage/LuggageTest.kt
@@ -2,6 +2,7 @@ package luggage
 
 import org.testng.annotations.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class LuggageTest {
 
@@ -18,6 +19,26 @@ class LuggageTest {
 
         assertEquals(1, parents.size)
         assertEquals(luggage.bags["blue"], parents.first())
+    }
+
+    @Test
+    fun getAllAncestors() {
+        val luggage = Luggage(
+            listOf(
+                Bag("blue", mapOf("red" to 1)),
+                Bag("pink", mapOf("red" to 1)),
+                Bag("green", mapOf("blue" to 1)),
+                Bag("yellow", mapOf()),
+                Bag("red", mapOf())
+            )
+        )
+        val ancestors = luggage.getAllAncestors("red")
+
+
+        assertEquals(3, ancestors.size)
+        assertTrue(ancestors.contains(luggage.bags["blue"]))
+        assertTrue(ancestors.contains(luggage.bags["pink"]))
+        assertTrue(ancestors.contains(luggage.bags["green"]))
     }
 
 }
