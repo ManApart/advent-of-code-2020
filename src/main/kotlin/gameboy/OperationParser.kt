2f45ab11c3638dc2e17e7b57a51bffa91b5c2a7a
ManApart <raustinkucera@gmail.com>
2020-12-08 16:20:55 -0500

    parse acc operation

--- src/main/kotlin/gameboy/OperationParser.kt
+++ src/main/kotlin/gameboy/OperationParser.kt
@@ -1,5 +1,12 @@
 package gameboy
 
 fun parseOperation(line: String): Operation {
-    return Operation(0 )
+    val parts = line.trim().split(" ")
+    val opName = parts.first()
+    val numberPart = parts.last().toInt()
+
+    return when (opName){
+        "acc" -> Operation(numberPart, 1)
+        else -> Operation(0, 1)
+    }
 }
--- src/test/kotlin/gameboy/OperationParserTest.kt
+++ src/test/kotlin/gameboy/OperationParserTest.kt
@@ -8,7 +8,21 @@ class OperationParserTest {
     @Test
     fun parseNoOp(){
         val result = parseOperation("nop +0")
-        val expected = Operation(0)
+        val expected = Operation(0, 1, false)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun parseIgnoresSpaces(){
+        val result = parseOperation("   nop +0  ")
+        val expected = Operation(0, 1, false)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun parseAcc(){
+        val result = parseOperation("acc +1")
+        val expected = Operation(1, 1, false)
         assertEquals(expected, result)
     }
 
