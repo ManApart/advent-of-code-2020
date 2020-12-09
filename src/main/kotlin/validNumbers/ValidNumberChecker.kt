3a18729cfaee3c0e5f9974910c1d04501df8e63a
ManApart <raustinkucera@gmail.com>
2020-12-09 18:57:40 -0500

    find simple invalid numbers

--- src/main/kotlin/validNumbers/ValidNumberChecker.kt
+++ src/main/kotlin/validNumbers/ValidNumberChecker.kt
@@ -1,5 +1,14 @@
 package validNumbers
 
-fun isValidNumber(numberToCheck: Int, lookBack: Int, numbers: List<Int>) : Boolean{
-    return true
+fun isValidNumber(numberToCheck: Int, lookBack: Int, numbers: List<Int>): Boolean {
+    val numbersToCheckAgainst = numbers.subList(numbers.size - lookBack, numbers.size)
+    numbersToCheckAgainst.forEach { first ->
+        numbersToCheckAgainst.forEach { second ->
+            if (first != second && first + second == numberToCheck){
+                return true
+            }
+        }
+    }
+
+    return false
 }
--- src/test/kotlin/validNumbers/ValidNumberCheckerTest.kt
+++ src/test/kotlin/validNumbers/ValidNumberCheckerTest.kt
@@ -2,6 +2,7 @@ package validNumbers
 
 import org.testng.Assert.assertTrue
 import org.testng.annotations.Test
+import kotlin.test.assertFalse
 
 class ValidNumberCheckerTest {
 
@@ -12,6 +13,15 @@ class ValidNumberCheckerTest {
         assertTrue(isValidNumber(4, 3, input))
         assertTrue(isValidNumber(5, 3, input))
         assertTrue(isValidNumber(5, 2, input))
+    }
+
+    @Test
+    fun invalidValues() {
+        val input = listOf(1,2,3)
+
+        assertFalse(isValidNumber(6, 3, input))
+        assertFalse(isValidNumber(4, 2, input))
+        assertFalse(isValidNumber(2, 3, input), "Can't use duplicate numbers")
     }
 
 
