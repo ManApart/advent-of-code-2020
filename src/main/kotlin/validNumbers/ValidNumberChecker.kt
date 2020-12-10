0a246803c49a3f4e98ad83453a63b6fbcb060877
ManApart <raustinkucera@gmail.com>
2020-12-09 19:12:15 -0500

    find first invalid number

--- src/main/kotlin/validNumbers/ValidNumberChecker.kt
+++ src/main/kotlin/validNumbers/ValidNumberChecker.kt
@@ -1,10 +1,20 @@
 package validNumbers
 
-fun isValidNumber(numberToCheck: Int, lookBack: Int, numbers: List<Int>): Boolean {
-    val numbersToCheckAgainst = numbers.subList(numbers.size - lookBack, numbers.size)
-    numbersToCheckAgainst.forEach { first ->
-        numbersToCheckAgainst.forEach { second ->
-            if (first != second && first + second == numberToCheck){
+fun findFirstInvalidNumber(lookBack: Int, numbers: List<Int>): Int {
+    for (i in lookBack..numbers.size) {
+        val possible = numbers[i]
+        val subsection = numbers.subList(i - lookBack, i)
+        if (!isValidNumber(possible, subsection)) {
+            return possible
+        }
+    }
+    return -1
+}
+
+fun isValidNumber(numberToCheck: Int, numbers: List<Int>): Boolean {
+    numbers.forEach { first ->
+        numbers.forEach { second ->
+            if (first != second && first + second == numberToCheck) {
                 return true
             }
         }
--- src/test/kotlin/validNumbers/ValidNumberCheckerTest.kt
+++ src/test/kotlin/validNumbers/ValidNumberCheckerTest.kt
@@ -1,6 +1,7 @@
 package validNumbers
 
 import org.testng.Assert.assertTrue
+import org.testng.AssertJUnit.assertEquals
 import org.testng.annotations.Test
 import kotlin.test.assertFalse
 
@@ -10,18 +11,65 @@ class ValidNumberCheckerTest {
     fun validValue() {
         val input = listOf(1,2,3)
 
-        assertTrue(isValidNumber(4, 3, input))
-        assertTrue(isValidNumber(5, 3, input))
-        assertTrue(isValidNumber(5, 2, input))
+        assertTrue(isValidNumber(4, input))
+        assertTrue(isValidNumber(5, input))
+    }
+
+    @Test
+    fun validValueWithDifferentLookback() {
+        val input = listOf(2,3)
+
+        assertTrue(isValidNumber(5, input))
     }
 
     @Test
     fun invalidValues() {
         val input = listOf(1,2,3)
 
-        assertFalse(isValidNumber(6, 3, input))
-        assertFalse(isValidNumber(4, 2, input))
-        assertFalse(isValidNumber(2, 3, input), "Can't use duplicate numbers")
+        assertFalse(isValidNumber(6, input))
+        assertFalse(isValidNumber(2, input), "Can't use duplicate numbers")
+    }
+
+    @Test
+    fun invalidValuesWithDifferentLookback() {
+        val input = listOf(2,3)
+
+        assertFalse(isValidNumber(4, input))
+    }
+
+    @Test
+    fun findInvalidCase() {
+        val input = listOf(0,2,3,4)
+
+        assertEquals(4, findFirstInvalidNumber(3,  input))
+    }
+
+    @Test
+    fun part1ExampleTest() {
+        val input = """
+            35
+            20
+            15
+            25
+            47
+            40
+            62
+            55
+            65
+            95
+            102
+            117
+            150
+            182
+            127
+            219
+            299
+            277
+            309
+            576
+        """.trimIndent().split("\n").map { it.toInt() }
+
+        assertEquals(127, findFirstInvalidNumber(5,  input))
     }
 
 
