7d23250b381776485e5099cd80750ff5f289f2ee
ManApart <raustinkucera@gmail.com>
2020-12-10 15:06:24 -0500

    find basic voltage counts

--- src/main/kotlin/adapters/AdapterPlanner.kt
+++ src/main/kotlin/adapters/AdapterPlanner.kt
@@ -3,3 +3,15 @@ package adapters
 fun findProductOfVoltageCounts(numbers: List<Int>) : Int {
     return 0
 }
+
+
+fun findVoltageCounts(numbers: List<Int>) : Map<Int, Int> {
+    val voltageCounts = mutableMapOf<Int, Int>()
+    numbers.windowed(2,1) {
+        val diff = it.last() - it.first()
+        voltageCounts.putIfAbsent(diff, 0)
+        voltageCounts[diff] = voltageCounts[diff]!! + 1
+    }
+
+    return voltageCounts
+}
--- src/test/kotlin/adapters/AdapterPlannerTest.kt
+++ src/test/kotlin/adapters/AdapterPlannerTest.kt
@@ -6,24 +6,41 @@ import kotlin.test.assertEquals
 class AdapterPlannerTest {
 
     @Test
-    fun part1Example1() {
+    fun findVoltageCounts() {
         val input = """
-            16
-            10
-            15
-            5
             1
-            11
-            7
-            19
-            6
-            12
+            2
+            3
             4
+            7
         """.trimIndent().split("\n").map { it.toInt() }
-        val voltageMath = findProductOfVoltageCounts(input)
+        val voltageMath = findVoltageCounts(input)
+        val expected = mapOf(1 to 3, 3 to 1)
 
-        assertEquals(5, voltageMath)
+        assertEquals(expected, voltageMath)
     }
+    
+    //reverse input
+
+//    @Test
+//    fun part1Example1() {
+//        val input = """
+//            16
+//            10
+//            15
+//            5
+//            1
+//            11
+//            7
+//            19
+//            6
+//            12
+//            4
+//        """.trimIndent().split("\n").map { it.toInt() }
+//        val voltageMath = findProductOfVoltageCounts(input)
+//
+//        assertEquals(5, voltageMath)
+//    }
 
 
 }
